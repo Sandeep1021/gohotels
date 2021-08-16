1a6ab3aea735ffdc2b009b2bf7ed7d4c66ce3120
Sandeep1922 <sandeeplmp9598@gmail.com>
2021-08-16 12:30:57 +0530

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,41 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-400">
+         <div classname="space-y-4 text-xs text-gray-800">
+             <h5 className="font-bold">ABOUT US</h5>
+             <p>How GoHotels Work?</p>
+             <p>Newsroom</p>
+             <p>Inverstors</p>
+             <p>GoHotels plus</p>
+             <p> GoHotels Luxe</p>
+         </div>  
+         <div classname="space-y-4 text-xs text-gray-800 ">
+             <h5 className="font-bold">ABOUT US</h5>
+             <p>Contact Us</p>
+             <p>heloo</p>
+             <p>Inverstors</p>
+             <p>GoHotels plus</p>
+             <p> GoHotels Luxe</p>
+         </div>  
+         <div classname="space-y-4  text-xs text-gray-800">
+             <h5 className="font-bold">ABOUT US</h5>
+             <p>How GoHotels Work</p>
+             <p>Newsroom</p>
+             <p>Inverstors</p>
+             <p>GoHotels plus</p>
+             <p> GoHotels Luxe</p>
+         </div>  
+         <div classname="space-y-4  text-xs text-gray-800">
+             <h5 className="font-bold">ABOUT US</h5>
+             <p>How GoHotels Work</p>
+             <p>Newsroom</p>
+             <p>Inverstors</p>
+             <p>GoHotels plus</p>
+             <p> GoHotels Luxe</p>
+         </div>  
+        
+        </div>
+    )
+}
+
+export default Footer
